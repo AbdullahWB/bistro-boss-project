c0d6e1051c67a1504a0c8dc2a908380d0f0f5c37
AbdullahWB <abdullah917828@gmail.com>
2023-05-30 01:15:12 +0600

    make jwt token

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from '../Firebase/Firebase.config';
+import axios from 'axios';
 
 
 export const AuthContext = createContext(null)
@@ -10,14 +11,14 @@ const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
     const googleProvider = new GoogleAuthProvider()
-    
 
-    const createUser = (email, password) => { 
+
+    const createUser = (email, password) => {
         setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
-    const signIn = (email, password) => { 
+    const signIn = (email, password) => {
         setLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
     }
@@ -43,12 +44,26 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             console.log('current user', currentUser);
+
+
+            // get and set token
+            if (currentUser) {
+                axios.post('http://localhost:3000/jwt', { email: currentUser.email })
+                    .then(data => {
+                        console.log(data.data.token);
+                        localStorage.setItem('access-token', data.data.token)
+                    })
+            } else {
+                localStorage.removeItem('access-token')
+            }
+
+
             setLoading(false);
         })
         return () => {
             return unsubscribe();
         }
-    },[])
+    }, [])
 
 
     const authInfo = {
@@ -63,7 +78,7 @@ const AuthProvider = ({ children }) => {
 
     return (
         <AuthContext.Provider value={authInfo}>
-           {children}
+            {children}
         </AuthContext.Provider>
     );
 };
